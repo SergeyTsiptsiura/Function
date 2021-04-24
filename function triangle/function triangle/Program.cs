ea2781f6ff5c38048d026d265f29815f32dc182c
SERG-PC\SERG <tspt.sergey@gmail.com>
2021-04-24 19:57:56 +0300

    24.04.2021

---
+++ function triangle/function triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Some()
+        {
+            Console.Write("Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int j = height; j > i; j--)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+
+        }
+        static void Main(string[] args)
+        {
+            Some();
+            Console.WriteLine();
+            Some();
+            Console.WriteLine();
+            Some();
+            Console.WriteLine();
+        }
+    }
+}
